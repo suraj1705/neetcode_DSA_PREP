ecfd39b389f6af899246477fcf087032b85435e8
SURAJ SINGH <77821599+suraj1705@users.noreply.github.com>
2026-08-20 04:49:05 -0700

    Add: guess-number-higher-or-lower - submission-1

---
+++ Data Structures & Algorithms/guess-number-higher-or-lower/submission-1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int guessNumber(int n) {
+        int l = 1, r = n;
+        while (true) {
+            int m = l + (r - l) / 2;
+            int res = guess(m);
+            if (res > 0) {
+                l = m + 1;
+            } else if (res < 0) {
+                r = m - 1;
+            } else {
+                return m;
+            }
+        }
+    }
+};
